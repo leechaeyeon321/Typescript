f50f8716084d6801dedd1afc652bf705582a635b
leechaeyeon321 <leechaeyeon321@gmail.com>
2023-04-08 23:58:12 +0900

    feat: typescript

--- todo-list-typescript/src/TodoListItem.jsx
+++ todo-list-typescript/src/TodoListItem.jsx
@@ -6,7 +6,11 @@ export default function TodoListItem() {
         <h1>Todo List</h1>
         <ul>
             <li>Item 1</li>
-            <li>Item 1</li>
+            <li>Item 2</li>
+        </ul>
+        <ul>
+            <li>Item 3</li>
+            <li>Item 4</li>
         </ul>
         <input type="text" placeholder='Add todo item'/>
         <button>Add</button>
